0430e9ed642a052ce29fe51a326e359e3d317a7e
Williams Joseph <wj882244@gmail.com>
2023-12-19 00:27:00 +0100

    fixed bug

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,23 +3,7 @@ import { FaDiscord } from "react-icons/fa";
 import { BiSolidCustomize } from "react-icons/bi";
 import { GiLaptop } from "react-icons/gi";
 import { FaBug } from "react-icons/fa";
-import { motion } from "framer-motion";
 import Footer from "./General Component/Footer";
-
-const draw = {
-  hidden: { pathLength: 0, opacity: 0 },
-  visible: (i) => {
-    const delay = 1 + i * 0.5;
-    return {
-      pathLength: 1,
-      opacity: 1,
-      transition: {
-        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-        opacity: { delay, duration: 0.01 },
-      },
-    };
-  },
-};
 
 export default function Home() {
   return (
