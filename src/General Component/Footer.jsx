eab14d9afe1e1a4415f7fa5eb09a63ca7d967dd0
Williams Joseph <wj882244@gmail.com>
2023-12-19 00:18:21 +0100

    update

--- src/General Component/Footer.jsx
+++ src/General Component/Footer.jsx
@@ -3,9 +3,11 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-[#13072E] py-10">
+    <footer className="bg-gradient-to-t from-[#13072E] to-[#402182] py-10 mt-10">
       <p className="text-center text-white text-xl">Astro Studios</p>
-      <p className="text-center mt-4 text-white ">Copyright&copy; {currentYear} By Dev Joe</p>
+      <p className="text-center mt-4 text-white text-sm">
+        Copyright&copy; {currentYear} By Dev Joe
+      </p>
     </footer>
   );
 }
