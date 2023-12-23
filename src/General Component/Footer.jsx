f7b7f0dfe116292b7f0c16002e05989d152ef3db
Jxni.08 <143966310+Joni0808@users.noreply.github.com>
2023-12-23 15:18:43 +0100

    Update (#1)

--- src/General Component/Footer.jsx
+++ src/General Component/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
 
   return (
     <footer className="bg-gradient-to-t from-[#13072E] to-[#402182] py-10 mt-10 select-none">
-      <p className="text-center text-white text-xl">Astro Studios</p>
+      <p className="text-center text-white text-xl">Website made by Joe</p>
       <p className="text-center mt-4 text-white text-sm">
-        Copyright&copy; {currentYear} By Dev Joe
+        Copyright&copy; {currentYear} Joni & Astro
       </p>
     </footer>
   );
